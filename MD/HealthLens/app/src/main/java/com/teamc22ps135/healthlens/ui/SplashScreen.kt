dd732fc28336dd7ddfe23dfa0d6a706c52744570
Tegar Naufal Hanip <tegarnaufalh@gmail.com>
2022-05-17 11:31:41 +0700

    Add UI Splash Screen 2 (Motto)

--- MD/HealthLens/app/src/main/java/com/teamc22ps135/healthlens/ui/SplashScreen.kt
+++ MD/HealthLens/app/src/main/java/com/teamc22ps135/healthlens/ui/SplashScreen.kt
@@ -1,14 +1,40 @@
 package com.teamc22ps135.healthlens.ui
 
 import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.view.WindowInsets
+import android.view.WindowManager
 import com.teamc22ps135.healthlens.R
 
 @SuppressLint("CustomSplashScreen")
+@Suppress("DEPRECATION")
 class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+        supportActionBar?.hide()
+
+        Handler().postDelayed({
+            val intent = Intent(this, LandingActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, DELAY_TIME)
+    }
+
+    companion object {
+        private const val DELAY_TIME = 2000L   // Delayed for 2 seconds.
     }
 }
