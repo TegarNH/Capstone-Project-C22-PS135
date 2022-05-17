f81aaf421f4cd02652392d9d28393e9da1a65905
Tegar Naufal Hanip <tegarnaufalh@gmail.com>
2022-05-17 12:43:26 +0700

    Add UI Splash Screen 1 (Starting App)

--- MD/HealthLens/app/src/main/java/com/teamc22ps135/healthlens/ui/SplashScreen.kt
+++ MD/HealthLens/app/src/main/java/com/teamc22ps135/healthlens/ui/SplashScreen.kt
@@ -8,33 +8,36 @@ import android.os.Bundle
 import android.os.Handler
 import android.view.WindowInsets
 import android.view.WindowManager
+import androidx.activity.viewModels
+import androidx.core.app.ActivityOptionsCompat
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.teamc22ps135.healthlens.R
+import com.teamc22ps135.healthlens.viewmodel.SplashViewModel
 
 @SuppressLint("CustomSplashScreen")
 @Suppress("DEPRECATION")
 class SplashScreen : AppCompatActivity() {
+    private val splashViewModel: SplashViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash_screen)
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-            window.insetsController?.hide(WindowInsets.Type.statusBars())
-        } else {
-            window.setFlags(
-                WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN
-            )
+        installSplashScreen().apply {
+            setKeepOnScreenCondition {
+                splashViewModel.isLoading.value
+            }
         }
+
+        setContentView(R.layout.activity_splash_screen)
         supportActionBar?.hide()
 
         Handler().postDelayed({
             val intent = Intent(this, LandingActivity::class.java)
-            startActivity(intent)
-            finish()
+            startActivity(intent, ActivityOptionsCompat.makeSceneTransitionAnimation(this@SplashScreen).toBundle())
         }, DELAY_TIME)
     }
 
     companion object {
-        private const val DELAY_TIME = 2000L   // Delayed for 2 seconds.
+        private const val DELAY_TIME = 3000L   // Delayed for 3 seconds.
     }
 }
