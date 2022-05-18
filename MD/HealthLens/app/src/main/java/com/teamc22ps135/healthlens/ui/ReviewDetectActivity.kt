b00f17f5f476278cb0d371f9310b8bd800ce418f
vina pc <vinna.mariska@gmail.com>
2022-05-18 12:30:40 +0700

    Add Detection Activity (Camera)

--- MD/HealthLens/app/src/main/java/com/teamc22ps135/healthlens/ui/ReviewDetectActivity.kt
+++ MD/HealthLens/app/src/main/java/com/teamc22ps135/healthlens/ui/ReviewDetectActivity.kt
@@ -1,5 +1,6 @@
 package com.teamc22ps135.healthlens.ui
 
+import android.Manifest
 import android.animation.AnimatorSet
 import android.animation.ObjectAnimator
 import androidx.appcompat.app.AppCompatActivity
@@ -13,5 +14,11 @@ class ReviewDetectActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_review_detect)
+    }
+
+    companion object {
+        const val CAMERA_X_RESULT = 200
+        private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
+        private const val REQUEST_CODE_PERMISSIONS = 10
     }
 }
