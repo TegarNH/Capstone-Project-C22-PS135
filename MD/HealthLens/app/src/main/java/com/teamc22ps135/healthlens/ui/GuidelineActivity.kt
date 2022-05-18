13f56884f3162ac5d096637e3ad94df0c032d0d6
vina pc <vinna.mariska@gmail.com>
2022-05-18 13:01:12 +0700

    Add Guideline Activity (Camera)

--- MD/HealthLens/app/src/main/java/com/teamc22ps135/healthlens/ui/GuidelineActivity.kt
+++ MD/HealthLens/app/src/main/java/com/teamc22ps135/healthlens/ui/GuidelineActivity.kt
@@ -1,12 +1,96 @@
 package com.teamc22ps135.healthlens.ui
 
+import android.content.Intent
+import android.content.Intent.ACTION_GET_CONTENT
+import android.content.pm.PackageManager
+import android.graphics.BitmapFactory
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import com.teamc22ps135.healthlens.R
+import com.teamc22ps135.healthlens.databinding.ActivityGuidelineBinding
+import com.teamc22ps135.healthlens.util.rotateBitmap
+import com.teamc22ps135.healthlens.util.uriToFile
+import java.io.File
 
 class GuidelineActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityGuidelineBinding
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (!allPermissionsGranted()) {
+                Toast.makeText(
+                    this,
+                    "Tidak mendapatkan permission.",
+                    Toast.LENGTH_SHORT
+                ).show()
+                finish()
+            }
+        }
+    }
+
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(baseContext, it) == PackageManager.PERMISSION_GRANTED
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_guideline)
     }
+
+    private fun startCameraX() {
+        val intent = Intent(this, DetectionActivity::class.java)
+        launcherIntentCameraX.launch(intent)
+    }
+
+    private fun startGallery() {
+        val intent = Intent()
+        intent.action = ACTION_GET_CONTENT
+        intent.type = "image/*"
+        val chooser = Intent.createChooser(intent, "Choose a Picture")
+        launcherIntentGallery.launch(chooser)
+    }
+
+    private val launcherIntentCameraX = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) {
+        if (it.resultCode == CAMERA_X_RESULT) {
+            val myFile = it.data?.getSerializableExtra("picture") as File
+            val isBackCamera = it.data?.getBooleanExtra("isBackCamera", true) as Boolean
+
+            val result = rotateBitmap(
+                BitmapFactory.decodeFile(myFile.path),
+                isBackCamera
+            )
+            // nampilin hasil gambar harusnya ke reviewdetection
+            binding.ivImage.setImageBitmap(result)
+        }
+    }
+
+    private val launcherIntentGallery = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+        if (result.resultCode == RESULT_OK) {
+            val selectedImg: Uri = result.data?.data as Uri
+            val myFile = uriToFile(selectedImg, this@GuidelineActivity)
+            // nampilin hasil gambar harusnya ke reviewdetection
+            binding.ivImage.setImageURI(selectedImg)
+        }
+    }
+
+    companion object {
+        const val CAMERA_X_RESULT = 200
+        private val REQUIRED_PERMISSIONS = arrayOf(android.Manifest.permission.CAMERA)
+        private const val REQUEST_CODE_PERMISSIONS = 10
+    }
+
 }
